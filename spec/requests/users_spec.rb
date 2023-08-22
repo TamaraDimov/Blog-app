908f5c8a469829f4304787601746e2e0f3ccf9f2
TamaraDimov <tamemojsoska@gmail.com>
2023-08-23 00:36:37 +0200

    fix linter error

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,7 +12,8 @@ end
 
 describe 'get user details from /users/:id' do
   it 'returns a successful response and includes user info' do
-    user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 0)
+    user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                       post_counter: 0)
     get "/users/#{user.id}"
 
     expect(response).to be_successful
