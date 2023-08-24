96a1c6dac2f62e676b4de140f338d108ef10fa17
TamaraDimov <tamemojsoska@gmail.com>
2023-08-24 14:50:54 +0200

    add views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  # belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
   has_many :comments
   has_many :likes
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Tom', photo: 'https://images.app.goo.gl/6QRPrnW9f92AsBvh8', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://images.app.goo.gl/6QRPrnW9f92AsBvh8', bio: 'Teacher from Poland.')
