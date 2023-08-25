74a80132331c3212a2126c56430488a6f90acd30
TamaraDimov <tamemojsoska@gmail.com>
2023-08-25 12:16:49 +0200

    add forms

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    @comment = @post.comments.build(author: @user, text: comments_params[:text])
+
+    if @comment.save
+      redirect_to user_post_path(@user, @post), notice: 'The comment is created'
+    else
+      redirect_to new_post_comment_path(@user, @post), alert: 'The comment was not created'
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:text)
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,34 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-    @posts = @user.posts.includes(:comments)
+    @posts = @user.posts.includes(:comments).order(created_at: :desc)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments).find(params[:id])
     @user = @post.author
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+    if @post.valid?
+      redirect_to user_post_path(current_user, @post), notice: 'The post has been created'
+    else
+      redirect_to new_user_post_path(current_user), alert: 'The post was not created'
+    end
+  end
+
+  private
+
+  def post_params
+    post = params.require(:post).permit(:title, :text)
+    post[:commentsCounter] = 0
+    post[:likesCounter] = 0
+    post
   end
 end
