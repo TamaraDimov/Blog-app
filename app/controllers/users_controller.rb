9bd14fbe6e9b18e568b3bdf6042883ef2f526827
TamaraDimov <tamemojsoska@gmail.com>
2023-08-28 15:41:50 +0200

    solve n+1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:posts).find(params[:id])
     @user_posts = @user.posts.includes(:comments)
     render :show
   end
