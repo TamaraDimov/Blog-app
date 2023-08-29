c1c6c7fe11eb849c3d9eb9e4ba68ff99d04e43ef
TamaraDimov <tamemojsoska@gmail.com>
2023-08-29 11:17:30 +0200

    update rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @users = User.all
     @post = Post.new
@@ -10,12 +10,12 @@ class UsersController < ApplicationController
     # @user = User.includes(:posts).find(params[:id])
     # @user_posts = @user.posts.includes(:comments)
     # render :show
-    if params[:id] == "sign_out"
+    if params[:id] == 'sign_out'
       sign_out_and_redirect
-      else
-        find_user
-      end
-end
+    else
+      find_user
+    end
+  end
 
   private
 
